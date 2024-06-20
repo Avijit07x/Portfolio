5bd036d83f04180890b33783d30ed6cc8a766e63
Avijit07x <deyavijit134@gmail.com>
2024-06-20 08:19:40 +0530

    fix : animation

--- src/app/error.js
+++ src/app/error.js
@@ -1,6 +1,10 @@
 "use client";
 const Error = () => {
-	return <div>error</div>;
+	return (
+		<div className="grid place-items-center">
+			<h1 className="text-red-500">Some Error Occurred</h1>
+		</div>
+	);
 };
 
 export default Error;
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Animation from "@/components/Animation";
 import Hero from "@/components/Hero";
 import LanguagesTools from "@/components/LanguagesTools";
 import MyWork from "@/components/MyWork";
@@ -9,7 +10,9 @@ const Page = () => {
 			<Navbar />
 			<main className="px-4 pt-8 lg:px-24 lg:pt-0 xl:px-36">
 				<Hero />
-				<LanguagesTools />
+				<Animation>
+					<LanguagesTools />
+				</Animation>
 				<MyWork />
 			</main>
 		</>
