8493d34c6ceb5129e3632029c540090d4fbb3d6d
Avijit07x <deyavijit134@gmail.com>
2024-06-20 09:15:53 +0530

    fix : scroll

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en" className="scroll-smooth scrollbar-thin">
+		<html lang="en">
 			<body className={`${inter.className} relative`}>
 				<div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
 				{children}
--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,17 +1,18 @@
 "use client";
 
 import { motion } from "framer-motion";
+import LanguagesTools from "./LanguagesTools";
 
-const Animation = ({ children }) => {
+const AnimatedLanguagesTools = () => {
 	return (
 		<motion.div
 			initial={{ opacity: 0, y: 80 }}
 			whileInView={{ opacity: 1, y: 0 }}
 			transition={{ ease: [0.25, 0.1, 0.25, 1], duration: 1 }}
 		>
-			{children}
+			<LanguagesTools />
 		</motion.div>
 	);
 };
 
-export default Animation;
+export default AnimatedLanguagesTools;
