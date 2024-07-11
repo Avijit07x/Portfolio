f259c397a86750c25fc44987b3dac8ae5676353e
Avijit07x <deyavijit134@gmail.com>
2024-07-11 12:10:30 +0530

    fix : api

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,27 @@ const nextConfig = {
 			},
 		],
 	},
+	async headers() {
+		return [
+			{
+				source: "/api/:path*",
+				headers: [
+					{
+						key: "Access-Control-Allow-Origin",
+						value: process.env.NEXT_PUBLIC_BASE_URL, // apni website ka URL yahan specify karo
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET,POST,PUT,DELETE,OPTIONS",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "X-Requested-With, Content-Type, Authorization",
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
