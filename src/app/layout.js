4ab745f0c4e3a25d8fa4b089ee55622be2f2781c
Avijit07x <deyavijit134@gmail.com>
2024-07-16 09:11:16 +0530

    fix : speed-insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
 import { Inter } from "next/font/google";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +19,6 @@ export default function RootLayout({ children }) {
 			<body className={`${inter.className} relative`}>
 				<div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
 				{children}
-				<SpeedInsights />
 			</body>
 		</html>
 	);
