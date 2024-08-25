2a7862e8e75c015b8f6b01eec423c6f5c621ff2b
Avijit Dey <deyavijit134@gmail.com>
2024-08-25 22:24:48 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,70 +1,43 @@
-"use client";
-
 import ContactMe from "@/components/ContactMe";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import LanguagesTools from "@/components/LanguagesTools";
 import MyWork from "@/components/MyWork";
 import Navbar from "@/components/Navbar";
-import { useEffect, useState } from "react";
 
-const Page = () => {
-	const [tools, setTools] = useState([]);
-	const [projects, setProjects] = useState([]);
-	const [userData, setUserData] = useState([]);
+export const getServerSideProps = async () => {
+	try {
+		const [toolsRes, projectsRes, userDataRes] = await Promise.all([
+			fetch(process.env.NEXT_PUBLIC_BASE_URL + "api/tools", {
+				headers: { "Content-Type": "application/json" },
+			}),
+			fetch(process.env.NEXT_PUBLIC_BASE_URL + "api/projects", {
+				headers: { "Content-Type": "application/json" },
+			}),
+			fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/user-data`, {
+				cache: "force-cache",
+				headers: { "Content-Type": "application/json" },
+			}),
+		]);
 
-	useEffect(() => {
-		const getTools = async () => {
-			try {
-				const res = await fetch(
-					process.env.NEXT_PUBLIC_BASE_URL + "api/tools",
-					{
-						headers: {
-							"Content-Type": "application/json",
-						},
-					},
-				);
-				const data = await res.json();
-				setTools(data);
-			} catch (error) {
-				console.log({ error: error.message });
-				return [];
-			}
-		};
-		const getProjects = async () => {
-			const res = await fetch(
-				process.env.NEXT_PUBLIC_BASE_URL + "api/projects",
-				{
-					headers: {
-						"Content-Type": "application/json",
-					},
-				},
-			);
-			const data = await res.json();
-			setProjects(data.reverse());
-		};
-		const getUserData = async () => {
-			try {
-				const res = await fetch(
-					`${process.env.NEXT_PUBLIC_BASE_URL}api/user-data`,
-					{
-						cache: "force-cache",
-						headers: {
-							"Content-Type": "application/json",
-						},
-					},
-				);
-				const data = await res.json();
-				setUserData(data[0]);
-			} catch (error) {
-				console.log({ error: error.message });
-			}
-		};
-		getUserData();
-		getTools();
-		getProjects();
-	}, []);
+		const tools = await toolsRes.json();
+		const projects = await projectsRes.json();
+		const userData = await userDataRes.json();
 
+		return {
+			props: {
+				tools,
+				projects: projects.reverse(),
+				userData: userData[0],
+			},
+		};
+	} catch (error) {
+		console.error(error);
+		return { props: { tools: [], projects: [], userData: {} } };
+	}
+};
+
+const Page = ({ tools, projects, userData }) => {
 	return (
 		<>
 			<header>
